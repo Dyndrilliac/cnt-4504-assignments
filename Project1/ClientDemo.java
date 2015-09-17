af7a9ff748d7ceb5dd2f8a521c4c8632472aa1f8
Matthew Boyette <Dyndrilliac@gmail.com>
2015-09-17 13:38:32 -0400

    Started Redesigning Project 1

--- Project1/ClientDemo.java
+++ Project1/ClientDemo.java
@@ -1,351 +1,71 @@
 /*
-	Title: CNT 4504 Project 1 - Client Demo
-	Author: Matthew Boyette
-	Date: 1/12/2014
-	
-	This code implements the client application.
-*/
+ * Title: CNT 4504 Project 1 - Client Demo
+ * Author: Matthew Boyette
+ * Date: 1/12/2014
+ * 
+ * This code implements the client application.
+ */
 
-import api.gui.*;
-import api.util.*;
-import CNT4504.Project1Code.CNT4504Project1Code.*;
+package Project1;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
+import api.gui.swing.ApplicationWindow;
+import api.util.Support;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.KeyStroke;
-
-public class ClientDemo
+public final class ClientDemo
 {
-	private final static Font	textFont	= new Font("Lucida Console", Font.PLAIN, 14);
-	
-	private static boolean							debugMode			= false;
-	private static String							hostName			= null;
-	private static EventHandler						myActionPerformed	= null;
-	private static EventHandler						myDrawGUI			= null;
-	private static Networking.SimpleClientThread	myThread			= null;
-	private static ApplicationWindow				myWindow			= null;
-	private static int								portNumber			= 0;
-	
-	public static synchronized RichTextPane getOutput()
+	public final static void main(final String[] args)
 	{
-		RichTextPane output = null;
-		
-		if (ClientDemo.myWindow != null)
-		{
-			for (int i = 0; i < ClientDemo.myWindow.getElements().size(); i++)
-			{
-				if (ClientDemo.myWindow.getElements().get(i) instanceof RichTextPane)
-				{
-					output = (RichTextPane)ClientDemo.myWindow.getElements().get(i);
-				}
-			}
-		}
-		
-		return output;
+		new ClientDemo();
 	}
 	
-	@SuppressWarnings("unchecked")
-	public static synchronized JComboBox<String> getInput()
+	private ClientThread[]		clients		= null;
+	private boolean				isDebugging	= false;
+	private ApplicationWindow	window		= null;
+	
+	public ClientDemo()
 	{
-		JComboBox<String> input = null;
+		final int numClients = Support.getIntegerInputString(this.getWindow(), "How many clients?", "Load");
+		final int remotePort = Support.getIntegerInputString(this.getWindow(), "Which remote port?", "Port");
+		final String remoteHost = Support.getInputString(this.getWindow(), "Which remote host?", "Host");
+		final String command = Support.getInputString(this.getWindow(), "What command to send?", "Command");
 		
-		if (ClientDemo.myWindow != null)
+		ClientThread[] clients = new ClientThread[numClients];
+		
+		for (int i = 0; i < numClients; i++)
 		{
-			for (int i = 0; i < ClientDemo.myWindow.getElements().size(); i++)
-			{
-				if (ClientDemo.myWindow.getElements().get(i) instanceof JComboBox<?>)
-				{
-					input = (JComboBox<String>)ClientDemo.myWindow.getElements().get(i);
-				}
-			}
+			clients[i] = new ClientThread(this.getWindow(), remoteHost, remotePort, command);
 		}
 		
-		return input;
+		this.setClients(clients);
 	}
 	
-	// Client application entry-point.
-	public static void main(final String[] args)
+	public final ClientThread[] getClients()
 	{
-		int choice = Support.promptDebugMode(ClientDemo.myWindow);
-		ClientDemo.debugMode = (choice == JOptionPane.YES_OPTION);
-		
-		// Define a self-contained ActionListener event handler.
-		ClientDemo.myActionPerformed = new EventHandler()
-		{
-			@SuppressWarnings("unchecked")
-			@Override
-			public final void run(final Object... arguments) throws IllegalArgumentException
-			{
-				if ((arguments.length <= 1) || (arguments.length > 2))
-				{
-					throw new IllegalArgumentException("myActionPerformed Error : incorrect number of arguments.");
-				}
-				else if (!(arguments[0] instanceof ActionEvent))
-				{
-					throw new IllegalArgumentException("myActionPerformed Error : argument[0] is of incorrect type.");
-				}
-				else if (!(arguments[1] instanceof ApplicationWindow))
-				{
-					throw new IllegalArgumentException("myActionPerformed Error : argument[1] is of incorrect type.");
-				}
-				
-				ActionEvent event = (ActionEvent)arguments[0];
-				ApplicationWindow window = (ApplicationWindow)arguments[1];
-				RichTextPane output = null;
-				JComboBox<String> input = null;
-				
-				for (int i = 0; i < window.getElements().size(); i++)
-				{
-					if (window.getElements().get(i) instanceof RichTextPane)
-					{
-						output = (RichTextPane)window.getElements().get(i);
-					}
-				}
-				for (int i = 0; i < window.getElements().size(); i++)
-				{
-					if (window.getElements().get(i) instanceof JComboBox<?>)
-					{
-						input = (JComboBox<String>)window.getElements().get(i);
-					}
-				}
-				
-				if (output != null)
-				{
-					/*
-						JDK 7 allows string objects as the expression in a switch statement.
-						This generally produces more efficient byte code compared to a chain of if statements.
-						http://docs.oracle.com/javase/7/docs/technotes/guides/language/strings-switch.html
-					*/
-					switch (event.getActionCommand())
-					{
-						case "Clear":
-							
-							output.clear();
-							break;
-						
-						case "Open":
-							
-							output.openFile();
-							break;
-						
-						case "Save":
-							
-							output.saveFile();
-							break;
-							
-						case "Enter":
-							
-							String s = (String)input.getSelectedItem();
-							JOptionPane.showMessageDialog(ClientDemo.myWindow, s);
-							
-							switch (s)
-							{
-								case "Host Date/Time":
-									
-									// TODO
-									break;
-									
-								case "Host Uptime":
-									
-									// TODO
-									break;
-									
-								case "Host Memory Use":
-									
-									// TODO
-									break;
-								case "Host Netstat":
-									
-									// TODO
-									break;
-								case "Host Current Users":
-									
-									// TODO
-									break;
-									
-								case "Host Running Processes":
-									
-									// TODO
-									break;
-									
-								case "Quit":
-									
-									System.exit(0);
-									break;
-									
-								default:
-									
-									break;
-								
-							}
-							break;
-							
-						default:
-							
-							break;
-					}
-				}
-			}
-		};
-		
-		// Define a self-contained interface construction event handler.
-		ClientDemo.myDrawGUI = new EventHandler()
-		{
-			@Override
-			public final void run(final Object... arguments) throws IllegalArgumentException
-			{
-				if (arguments.length <= 0)
-				{
-					throw new IllegalArgumentException("myDrawGUI Error : incorrect number of arguments.");
-				}
-				else if (!(arguments[0] instanceof ApplicationWindow))
-				{
-					throw new IllegalArgumentException("myDrawGUI Error : argument[0] is of incorrect type.");
-				}
-				
-				ApplicationWindow window = (ApplicationWindow)arguments[0];
-				Container contentPane = window.getContentPane();
-				JMenuBar menuBar = new JMenuBar();
-				JMenu fileMenu = new JMenu("File");
-				JMenuItem clearOption = new JMenuItem("Clear");
-				JMenuItem openOption = new JMenuItem("Open");
-				JMenuItem saveOption = new JMenuItem("Save");
-				RichTextPane outputBox = new RichTextPane(window, true, window.isDebugging(), ClientDemo.textFont);
-				JComboBox<String> inputBox = new JComboBox<String>();
-				JButton btnEnter = new JButton("Enter");
-				
-				fileMenu.setFont(ClientDemo.textFont);
-				clearOption.setFont(ClientDemo.textFont);
-				openOption.setFont(ClientDemo.textFont);
-				saveOption.setFont(ClientDemo.textFont);
-				inputBox.setEditable(false);
-				inputBox.setFont(ClientDemo.textFont);
-				
-				fileMenu.setMnemonic('F');
-				openOption.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.Event.CTRL_MASK));
-				openOption.setMnemonic('O');
-				saveOption.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.Event.CTRL_MASK));
-				saveOption.setMnemonic('S');
-				clearOption.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.Event.CTRL_MASK));
-				clearOption.setMnemonic('C');
-				
-				contentPane.setLayout(new BorderLayout());
-				clearOption.addActionListener(window);
-				fileMenu.add(clearOption);
-				openOption.addActionListener(window);
-				fileMenu.add(openOption);
-				saveOption.addActionListener(window);
-				fileMenu.add(saveOption);
-				menuBar.add(fileMenu);
-				window.setJMenuBar(menuBar);
-				btnEnter.addActionListener(window);
-				
-				JScrollPane outputPanel = new JScrollPane(outputBox);
-				JPanel inputPanel = new JPanel();
-				
-				inputPanel.setLayout(new FlowLayout());
-				inputPanel.add(inputBox);
-				inputPanel.add(btnEnter);
-				contentPane.add(outputPanel, BorderLayout.CENTER);
-				contentPane.add(inputPanel, BorderLayout.SOUTH);
-				window.getElements().add(outputBox);
-				window.getElements().add(inputBox);
-				
-				window.getRootPane().setDefaultButton(btnEnter);
-				
-				inputBox.addItem("Host Date/Time");
-				inputBox.addItem("Host Uptime");
-				inputBox.addItem("Host Memory Use");
-				inputBox.addItem("Host Netstat");
-				inputBox.addItem("Host Current Users");
-				inputBox.addItem("Host Running Processes");
-				inputBox.addItem("Quit");
-				inputBox.setSelectedIndex(0);
-			}
-		};
-		
-		try
-		{
-			if (args.length > 0)
-			{
-				ClientDemo.hostName = args[0];
-				ClientDemo.setRemotePort();
-				ClientDemo.reset();
-			}
-			else
-			{
-				throw new Exception("User did not supply a remote hostname via the command-line!");
-			}
-		}
-		catch (Exception e)
-		{
-			Support.displayException(ClientDemo.myWindow, e, true);
-			e.printStackTrace();
-		}
+		return this.clients;
 	}
 	
-	public static synchronized void reset()
+	public final ApplicationWindow getWindow()
 	{
-		if (ClientDemo.myWindow != null)
-		{
-			ClientDemo.myWindow.dispose();
-			ClientDemo.myWindow = null;
-		}
-		
-		if (ClientDemo.myThread != null)
-		{
-			ClientDemo.myThread.close();
-			ClientDemo.myThread = null;
-		}
-		
-		String clientTitle = "Client Demo - Remote Host: " + ClientDemo.hostName + " - Remote Port: " + ClientDemo.portNumber;
-		
-		ClientDemo.myWindow = new ApplicationWindow(null, clientTitle, new Dimension(600, 400), ClientDemo.debugMode, false,
-			ClientDemo.myActionPerformed, ClientDemo.myDrawGUI);
-		ClientDemo.myThread = new CNT4504Project1ClientThread(ClientDemo.hostName, ClientDemo.portNumber, ClientDemo.myWindow);
-		ClientDemo.myThread.start();
-		
-		while (ClientDemo.myThread.isConnected())
-		{
-			continue;
-		}
-		
-		ClientDemo.myThread = null;
-		ClientDemo.myWindow = null;
+		return this.window;
 	}
 	
-	public static synchronized void setRemoteHost()
+	public final boolean isDebugging()
 	{
-		String s = null;
-		
-		s = Support.getInputString(ClientDemo.myWindow, "Remote host?", "Set Remote Host");
-
-		ClientDemo.hostName = s;
+		return this.isDebugging;
 	}
 	
-	public static synchronized void setRemotePort()
+	protected final void setClients(final ClientThread[] clients)
 	{
-		String s = null;
-		
-		do
-		{
-			s = Support.getInputString(ClientDemo.myWindow, "Remote port?", "Set Remote Port");
-		}
-		while (Support.isStringParsedAsInteger(s) != true);
-		
-		ClientDemo.portNumber = Integer.parseInt(s);
+		this.clients = clients;
+	}
+	
+	protected final void setDebugging(final boolean isDebugging)
+	{
+		this.isDebugging = isDebugging;
+	}
+	
+	protected final void setWindow(final ApplicationWindow window)
+	{
+		this.window = window;
 	}
 }
--- Project1/ServerDemo.java
+++ Project1/ServerDemo.java
@@ -1,217 +1,60 @@
 /*
-	Title: CNT 4504 Project 1 - Server Demo
-	Author: Matthew Boyette
-	Date: 1/12/2014
-	
-	This code implements the server application.
-*/
+ * Title: CNT 4504 Project 1 - Server Demo
+ * Author: Matthew Boyette
+ * Date: 1/12/2014
+ * 
+ * This code implements the server application.
+ */
 
-import api.gui.*;
-import api.util.*;
-import CNT4504.Project1Code.CNT4504Project1Code.*;
+package Project1;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
+import api.gui.swing.ApplicationWindow;
+import api.util.Support;
 
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.KeyStroke;
-
-public class ServerDemo
+public final class ServerDemo
 {
-	private final static Font	textFont	= new Font("Lucida Console", Font.PLAIN, 14);
-	
-	private static boolean							debugMode			= false;
-	private static EventHandler						myActionPerformed	= null;
-	private static EventHandler						myDrawGUI			= null;
-	private static Networking.SimpleServerThread	myThread			= null;
-	private static ApplicationWindow				myWindow			= null;
-	private static int								portNumber			= 0;
-	
-	public static synchronized RichTextPane getOutput()
+	public final static void main(final String[] args)
 	{
-		RichTextPane output = null;
-		
-		if (ServerDemo.myWindow != null)
-		{
-			
-		}
-		
-		return output;
+		new ServerDemo();
 	}
 	
-	// Server application entry-point.
-	public static void main(final String[] args)
+	private boolean				isDebugging	= false;
+	private ServerThread		server		= null;
+	private ApplicationWindow	window		= null;
+	
+	public ServerDemo()
 	{
-		int choice = Support.promptDebugMode(ServerDemo.myWindow);
-		ServerDemo.debugMode = (choice == JOptionPane.YES_OPTION);
-		
-		// Define a self-contained ActionListener event handler.
-		ServerDemo.myActionPerformed = new EventHandler()
-		{
-			@Override
-			public final void run(final Object... arguments) throws IllegalArgumentException
-			{
-				if ((arguments.length <= 1) || (arguments.length > 2))
-				{
-					throw new IllegalArgumentException("myActionPerformed Error : incorrect number of arguments.");
-				}
-				else if (!(arguments[0] instanceof ActionEvent))
-				{
-					throw new IllegalArgumentException("myActionPerformed Error : argument[0] is of incorrect type.");
-				}
-				else if (!(arguments[1] instanceof ApplicationWindow))
-				{
-					throw new IllegalArgumentException("myActionPerformed Error : argument[1] is of incorrect type.");
-				}
-				
-				ActionEvent event = (ActionEvent)arguments[0];
-				ApplicationWindow window = (ApplicationWindow)arguments[1];
-				RichTextPane output = null;
-				
-				for (int i = 0; i < window.getElements().size(); i++)
-				{
-					if (window.getElements().get(i) instanceof RichTextPane)
-					{
-						output = (RichTextPane)window.getElements().get(i);
-					}
-				}
-				
-				if (output != null)
-				{
-					/*
-						JDK 7 allows string objects as the expression in a switch statement.
-						This generally produces more efficient byte code compared to a chain of if statements.
-						http://docs.oracle.com/javase/7/docs/technotes/guides/language/strings-switch.html
-					*/
-					switch (event.getActionCommand())
-					{
-						case "Clear":
-							
-							output.clear();
-							break;
-						
-						case "Open":
-							
-							output.openFile();
-							break;
-						
-						case "Save":
-							
-							output.saveFile();
-							break;
-						
-						default:
-							
-							break;
-					}
-				}
-			}
-		};
-		
-		// Define a self-contained interface construction event handler.
-		ServerDemo.myDrawGUI = new EventHandler()
-		{
-			@Override
-			public final void run(final Object... arguments) throws IllegalArgumentException
-			{
-				if (arguments.length <= 0)
-				{
-					throw new IllegalArgumentException("myDrawGUI Error : incorrect number of arguments.");
-				}
-				else if (!(arguments[0] instanceof ApplicationWindow))
-				{
-					throw new IllegalArgumentException("myDrawGUI Error : argument[0] is of incorrect type.");
-				}
-				
-				ApplicationWindow window = (ApplicationWindow)arguments[0];
-				Container contentPane = window.getContentPane();
-				JMenuBar menuBar = new JMenuBar();
-				JMenu fileMenu = new JMenu("File");
-				JMenuItem clearOption = new JMenuItem("Clear");
-				JMenuItem openOption = new JMenuItem("Open");
-				JMenuItem saveOption = new JMenuItem("Save");
-				RichTextPane outputBox = new RichTextPane(window, true, window.isDebugging(), ServerDemo.textFont);
-				JScrollPane outputPanel = new JScrollPane(outputBox);
-				
-				fileMenu.setFont(ServerDemo.textFont);
-				clearOption.setFont(ServerDemo.textFont);
-				openOption.setFont(ServerDemo.textFont);
-				saveOption.setFont(ServerDemo.textFont);
-				
-				fileMenu.setMnemonic('F');
-				openOption.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.Event.CTRL_MASK));
-				openOption.setMnemonic('O');
-				saveOption.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.Event.CTRL_MASK));
-				saveOption.setMnemonic('S');
-				clearOption.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.Event.CTRL_MASK));
-				clearOption.setMnemonic('C');
-				
-				clearOption.addActionListener(window);
-				fileMenu.add(clearOption);
-				openOption.addActionListener(window);
-				fileMenu.add(openOption);
-				saveOption.addActionListener(window);
-				fileMenu.add(saveOption);
-				menuBar.add(fileMenu);
-				
-				contentPane.setLayout(new BorderLayout());
-				contentPane.add(outputPanel, BorderLayout.CENTER);
-				window.setJMenuBar(menuBar);
-				window.getElements().add(outputBox);
-			}
-		};
-		
-		ServerDemo.setListeningPort();
-		ServerDemo.reset();
+		final int listeningPort = Support.getIntegerInputString(null, "Which port to use for listening?", "Port");
+		this.setServer(new ServerThread(this.getWindow(), listeningPort));
 	}
 	
-	public static synchronized void reset()
+	public final ServerThread getServer()
 	{
-		if (ServerDemo.myWindow != null)
-		{
-			ServerDemo.myWindow.dispose();
-			ServerDemo.myWindow = null;
-		}
-		
-		if (ServerDemo.myThread != null)
-		{
-			ServerDemo.myThread.close();
-			ServerDemo.myThread = null;
-		}
-		
-		String serverTitle = "Server Demo - Listening Port: " + ServerDemo.portNumber;
-		
-		ServerDemo.myWindow = new ApplicationWindow(null, serverTitle, new Dimension(600, 400), ServerDemo.debugMode, false,
-			ServerDemo.myActionPerformed, ServerDemo.myDrawGUI);
-		ServerDemo.myThread = new CNT4504Project1ServerThread(ServerDemo.portNumber, ServerDemo.myWindow);
-		ServerDemo.myThread.start();
-		
-		while (ServerDemo.myThread.isListening())
-		{
-			continue;
-		}
-		
-		ServerDemo.myThread = null;
-		ServerDemo.myWindow = null;
+		return this.server;
 	}
 	
-	public static synchronized void setListeningPort()
+	public final ApplicationWindow getWindow()
 	{
-		String s = null;
-		
-		do
-		{
-			s = Support.getInputString(ServerDemo.myWindow, "Listening port?", "Set Listening Port");
-		}
-		while (Support.isStringParsedAsInteger(s) != true);
-		
-		ServerDemo.portNumber = Integer.parseInt(s);
+		return this.window;
+	}
+	
+	public final boolean isDebugging()
+	{
+		return this.isDebugging;
+	}
+	
+	protected final void setDebugging(final boolean isDebugging)
+	{
+		this.isDebugging = isDebugging;
+	}
+	
+	protected final void setServer(final ServerThread server)
+	{
+		this.server = server;
+	}
+	
+	protected final void setWindow(final ApplicationWindow window)
+	{
+		this.window = window;
 	}
 }
