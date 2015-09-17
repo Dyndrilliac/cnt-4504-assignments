1fc5b55d75b595b3ba390fe317426a973f5d2930
Matthew Boyette <Dyndrilliac@gmail.com>
2015-09-17 13:38:57 -0400

    Uploaded Stub For Project 2

---
+++ Project2/NetworkingCode.java
@@ -0,0 +1,14 @@
+/*
+ * Title: CNT 4504 Project 2 - Core Networking
+ * Author: Matthew Boyette
+ * Date: 4/07/2014
+ * 
+ * This code implements the core networking functions needed by the client and server applications.
+ */
+
+package Project2;
+
+public class NetworkingCode
+{
+	// TODO
+}
